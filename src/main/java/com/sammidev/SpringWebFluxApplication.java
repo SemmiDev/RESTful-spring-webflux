088b5ed30b6ab9f58ebff368c504451dd4cd2979
SemmiDev <sammidev4@gmail.com>
2021-01-06 07:32:11 +0700

    Add email & phone number validator, student identifier generator, and update&create request

--- src/main/java/com/sammidev/SpringWebFluxApplication.java
+++ src/main/java/com/sammidev/SpringWebFluxApplication.java
@@ -2,7 +2,6 @@ package com.sammidev;
 
 // © 2021 sammidev
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,18 +13,19 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Random;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.MediaType.*;
@@ -50,21 +50,63 @@ class Student {
 	private String nim;
 	private String email;
 	private String phone;
+	private Path path;
 
 	private Date createdAt;
 	private Date updatedAt;
+}
 
-	public Student(String name, String nim, String email, String phone, Date createdAt, Date updatedAt) {
-		this.name = name;
-		this.nim = nim;
-		this.email = email;
-		this.phone = phone;
-		this.createdAt = createdAt;
-		this.updatedAt = updatedAt;
-	}
+enum Path {
+	SBMPTN,SNMPTN,MANDIRI
+}
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+class StudentCreateRequest {
+	private String name;
+	private String email;
+	private String phone;
+	private Path path;
+}
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+class StudentUpdateRequest {
+	private String name;
+	private String email;
+	private String phone;
 }
 
 interface StudentRepository extends ReactiveMongoRepository<Student, String> {}
+
+@Component
+class EmailValidator implements Predicate<String> {
+
+	private static final Predicate<String> IS_EMAIL_VALID =
+			Pattern.compile(
+					"^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
+					Pattern.CASE_INSENSITIVE
+			).asPredicate();
+
+	@Override
+	public boolean test(String email) {
+		return IS_EMAIL_VALID.test(email);
+	}
+}
+
+@Component
+class PhoneNumberValidator implements Predicate<String> {
+
+	private static final Predicate<String> IS_PHONENUMBER_VALID = Pattern.compile("^\\d{12}$").asPredicate();
+	@Override
+	public boolean test(String phoneNumber) {
+		return phoneNumber.startsWith("08") && IS_PHONENUMBER_VALID.test(phoneNumber);
+	}
+}
 
 @RestController
 class StudentController {
@@ -85,36 +127,58 @@ class StudentController {
 	}
 
 	@PostMapping("/students")
-	public Mono<Student> createStudent(@Valid @RequestBody Student student) {
-		var studentUpload = new Student(
-				student.getName(),
-				nimgenerator(),
-				student.getEmail(),
-				student.getPhone(),
-				new Date(),
-				null);
-		return studentRepository.save(studentUpload);
-	}
+	public Mono<Student> createStudent(@Valid @RequestBody StudentCreateRequest student) {
 
-	private String nimgenerator() {
-		LocalDate date = LocalDate.now();
-		var format = date.format(DateTimeFormatter.BASIC_ISO_DATE);
-		return format + new Date().toString();
+		Calendar calendar = Calendar.getInstance();
+		String uniqueNumber1 = String.format("%04d", new Random().nextInt(10000));
+		String uniqueNumber2 = String.format("%04d", new Random().nextInt(10000));
+
+		final String sbmptn = "311";
+		final String snmptn = "312";
+		final String mandiri = "313";
+
+		String nim = null;
+		if (student.getPath().toString().equals(Path.SBMPTN.name())) {
+			nim = sbmptn + uniqueNumber1 + uniqueNumber2;
+		}else if (student.getPath().toString().equals(Path.SNMPTN.name())) {
+			nim = snmptn + uniqueNumber1 + uniqueNumber2;
+		}else if (student.getPath().toString().equals(Path.MANDIRI.name())) {
+			nim = snmptn + uniqueNumber1 + uniqueNumber2;
+		}
+
+		Student studentResult = null;
+
+		if (new EmailValidator().test(student.getEmail()) && new PhoneNumberValidator().test(student.getPhone())) {
+			studentResult = Student.builder()
+					.name(student.getName())
+					.nim(nim)
+					.path(student.getPath())
+					.email(student.getEmail())
+					.phone(student.getPhone())
+					.createdAt(new Date())
+					.updatedAt(null)
+					.build();
+			return studentRepository.save(studentResult);
+		}
+		throw new IllegalArgumentException("phone number atau email anda tidak valid");
 	}
 
 	@PutMapping("/students/{id}")
 	public Mono<ResponseEntity<Student>> updateStudent(@PathVariable(value = "id") String studentId,
-												   @Valid @RequestBody Student studentRequest) {
-		return studentRepository.findById(studentId)
-				.flatMap(existingStudent -> {
-					existingStudent.setName(studentRequest.getName());
-					existingStudent.setEmail(studentRequest.getEmail());
-					existingStudent.setPhone(studentRequest.getPhone());
-					existingStudent.setUpdatedAt(new Date());
-					return studentRepository.save(existingStudent);
-				})
-				.map(updatedStudent -> new ResponseEntity<>(updatedStudent, OK))
-				.defaultIfEmpty(new ResponseEntity<>(NOT_FOUND));
+												   @Valid @RequestBody StudentUpdateRequest studentRequest) {
+		if (new EmailValidator().test(studentRequest.getEmail()) && new PhoneNumberValidator().test(studentRequest.getPhone())) {
+			return studentRepository.findById(studentId)
+					.flatMap(existingStudent -> {
+						existingStudent.setName(studentRequest.getName());
+						existingStudent.setEmail(studentRequest.getEmail());
+						existingStudent.setPhone(studentRequest.getPhone());
+						existingStudent.setUpdatedAt(new Date());
+						return studentRepository.save(existingStudent);
+					})
+					.map(updatedStudent -> new ResponseEntity<>(updatedStudent, OK))
+					.defaultIfEmpty(new ResponseEntity<>(NOT_FOUND));
+		}
+		throw new IllegalArgumentException("phone number atau email anda tidak valid");
 	}
 
 	@DeleteMapping("/students/{id}")
--- src/test/java/com/sammidev/StudentControllerTest.java
+++ src/test/java/com/sammidev/StudentControllerTest.java
@@ -1,7 +1,5 @@
 package com.sammidev;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class StudentControllerTest {
-
 }
